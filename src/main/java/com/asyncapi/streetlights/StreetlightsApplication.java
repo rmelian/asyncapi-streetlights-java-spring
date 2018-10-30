5ec3208c1a97a6f854b1fb611e3a8ec29493150d
Raisel Melian <raisel.melian@viacom.com>
2018-10-30 11:30:23 -0400

    first commit

---
+++ src/main/java/com/asyncapi/streetlights/StreetlightsApplication.java
@@ -0,0 +1,12 @@
+package com.asyncapi.streetlights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetlightsApplication.class, args);
+	}
+}
